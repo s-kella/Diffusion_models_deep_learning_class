efa8de3becbeb9d916426319dccfabfd6e90a349
Alexandra Bolotina <alexa@alexas-macbook.home>
2025-11-22 17:56:00 +0100

    add sigma sampling function

--- train.py
+++ train.py
@@ -15,3 +15,7 @@ def c_skip(sigma, sigma_data):
 
 def c_noise(sigma):
     return sigma.log() / 4
+
+
+def sample_sigma(n, device, loc=-1.2, scale=1.2, sigma_min=2e-3, sigma_max=80):
+    return (torch.randn(n, device=device) * scale + loc).exp().clip(sigma_min, sigma_max)
