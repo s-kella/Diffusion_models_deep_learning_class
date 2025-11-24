a5148001f722bc0f1a534a7abf07ffcfb6a12af3
Alexandra Bolotina <alexa@alexas-macbook.home>
2025-11-24 16:18:00 +0100

    integrate conditioning into model

--- model.py
+++ model.py
@@ -14,14 +14,18 @@ class Model(nn.Module):
         super().__init__()
         self.noise_emb = NoiseEmbedding(cond_channels)
         self.conv_in = nn.Conv2d(image_channels, nb_channels, kernel_size=3, padding=1)
-        self.blocks = nn.ModuleList([ResidualBlock(nb_channels) for _ in range(num_blocks)])
+        self.blocks = nn.ModuleList([ResidualBlock(nb_channels, cond_channels) for _ in range(num_blocks)])
         self.conv_out = nn.Conv2d(nb_channels, image_channels, kernel_size=3, padding=1)
+
+        # initialize final conv to zero
+        nn.init.zeros_(self.conv_out.weight)
+        nn.init.zeros_(self.conv_out.bias)
 
     def forward(self, noisy_input: torch.Tensor, c_noise: torch.Tensor) -> torch.Tensor:
         cond = self.noise_emb(c_noise)
         x = self.conv_in(noisy_input)
         for block in self.blocks:
-            x = block(x)
+            x = block(x, cond)
         return self.conv_out(x)
 
 
@@ -56,14 +60,18 @@ class ConditionalBatchNorm2d(nn.Module):
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, nb_channels: int) -> None:
+    def __init__(self, nb_channels: int, cond_channels: int) -> None:
         super().__init__()
-        self.norm1 = nn.BatchNorm2d(nb_channels)
+        self.norm1 = ConditionalBatchNorm2d(nb_channels, cond_channels)
         self.conv1 = nn.Conv2d(nb_channels, nb_channels, kernel_size=3, stride=1, padding=1)
-        self.norm2 = nn.BatchNorm2d(nb_channels)
+        self.norm2 = ConditionalBatchNorm2d(nb_channels, cond_channels)
         self.conv2 = nn.Conv2d(nb_channels, nb_channels, kernel_size=3, stride=1, padding=1)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        y = self.conv1(F.relu(self.norm1(x)))
-        y = self.conv2(F.relu(self.norm2(y)))
+        # initialize second conv to zero
+        nn.init.zeros_(self.conv2.weight)
+        nn.init.zeros_(self.conv2.bias)
+
+    def forward(self, x: torch.Tensor, cond: torch.Tensor) -> torch.Tensor:
+        y = self.conv1(F.relu(self.norm1(x, cond)))
+        y = self.conv2(F.relu(self.norm2(y, cond)))
         return x + y
