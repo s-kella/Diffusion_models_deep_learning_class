df92f30f4bcf2e31eafb973a544b97c1d8eab42d
Alexandra Bolotina <alexa@Alexas-MacBook.local>
2025-12-09 18:23:00 +0100

    make compatable with 3.9

--- data.py
+++ data.py
@@ -25,22 +25,20 @@ def load_dataset_and_make_dataloaders(
 
 def load_dataset(dataset_name='FashionMNIST', root_dir='data') -> Tuple[Dataset, Dataset, DataInfo]:
 
-    match dataset_name:
-        
-        case 'FashionMNIST':
-            t = T.Compose([T.ToTensor(), T.Pad(2), T.Normalize(mean=(0.5,), std=(0.5,))])
-            train_dataset = FashionMNIST(root_dir, download=True, transform=t)
-            train_dataset, valid_dataset = random_split(train_dataset, [50000, 10000])
-            num_classes = 10
-        
-        case 'CelebA':
-            t = T.Compose([T.ToTensor(), T.CenterCrop(178), T.Resize(128, antialias=True), T.Normalize(mean=(0.5,), std=(0.5,))])
-            train_dataset = CelebA(root_dir, download=True, transform=t)
-            train_dataset, valid_dataset = random_split(train_dataset, [150000, 12770])
-            num_classes = None
-        
-        case other:
-            raise RuntimeError('Unknown dataset: ' + other)
+    if dataset_name == 'FashionMNIST':
+        t = T.Compose([T.ToTensor(), T.Pad(2), T.Normalize(mean=(0.5,), std=(0.5,))])
+        train_dataset = FashionMNIST(root_dir, download=True, transform=t)
+        train_dataset, valid_dataset = random_split(train_dataset, [50000, 10000])
+        num_classes = 10
+
+    elif dataset_name == 'CelebA':
+        t = T.Compose([T.ToTensor(), T.CenterCrop(178), T.Resize(128, antialias=True), T.Normalize(mean=(0.5,), std=(0.5,))])
+        train_dataset = CelebA(root_dir, download=True, transform=t)
+        train_dataset, valid_dataset = random_split(train_dataset, [150000, 12770])
+        num_classes = None
+
+    else:
+        raise RuntimeError('Unknown dataset: ' + dataset_name)
 
     x, _ = next(iter(DataLoader(train_dataset, batch_size=10000, shuffle=True)))
     _, c, h, w = x.size()
